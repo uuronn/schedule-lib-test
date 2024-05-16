35dc41a51995e810c5693c8fdedc26ccb1ce5fc5
uuronn <80239312+uuronn@users.noreply.github.com>
2024-05-16 20:29:36 +0900

    Initial commit

---
+++ src/Home.tsx
@@ -0,0 +1,15 @@
+import { css } from "@emotion/react";
+
+function Home() {
+  return (
+    <div
+      css={css`
+        background: pink;
+      `}
+    >
+      hello world
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Root.tsx
@@ -0,0 +1,30 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements
+} from "react-router-dom";
+import Home from "./Home";
+import "./reset.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+    </Route>
+  )
+);
+
+const root = document.getElementById("root");
+
+if (root) {
+  const app = createRoot(root);
+
+  app.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+}
